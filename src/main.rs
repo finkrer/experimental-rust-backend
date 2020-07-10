609b86822a660a8b6f66eb36ce48694b86239c3c
finkrer <finkrer@gmail.com>
2020-07-10 15:16:16 +0500

    Move default headers into a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod headers;
 mod routes;
 mod ssl;
 mod templates;
@@ -10,14 +11,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Compress::default())
-            .wrap(middleware::DefaultHeaders::new()
-                .header("Strict-Transport-Security", "max-age=63072000; includeSubdomains; preload")
-                .header("X-Frame-Options", "SAMEORIGIN")
-                .header("X-Content-Type-Options", "nosniff")
-                .header("X-XSS-Protection", "1; mode=block")
-                .header("Content-Security-Policy", "script-src 'self'; object-src 'none'; img-src 'self' data:; style-src 'self' https://fonts.googleapis.com; base-uri 'none'; form-action 'none'; frame-ancestors 'self'; require-trusted-types-for 'script';")
-                .header("Referrer-Policy", "no-referrer")
-                .header("Feature-Policy", "vibrate 'self'"))
+            .wrap(headers::get_default())
             .wrap(RedirectSchemeBuilder::new().build())
             .wrap(middleware::NormalizePath)
             .configure(routes::config)
